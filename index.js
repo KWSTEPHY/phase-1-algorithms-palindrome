fa64155cbf011007b773f0d189ef543571dbd92a
stephy <stephshiro35@gmail.com>
2024-04-09 13:15:01 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
+  function isPalindrome(word) {
+    // Convert the string to lowercase
+    str = word.toLowerCase();
+
+    // Initialize pointers for the start and end of the string
+    let start = 0;
+    let end = word.length - 1;
+
+    // Iterate over the string until the pointers meet
+    while (start < end) {
+        // If characters at the current positions don't match, return false
+        if (word[start] !== word[end]) {
+            return false;
+        }
+        // Move the pointers inward
+        start++;
+        end--;
+    }
+    // If the loop completes without returning false, the string is a palindrome
+    return true;
 }
+
 
 /* 
   Add your pseudocode here
